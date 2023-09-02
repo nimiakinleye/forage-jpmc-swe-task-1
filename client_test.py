41d452ebc175adc6675ebca0e469f4b6f73a1951
Toluwanimi Akinleye <toluwanimiakinleye01@gmail.com>
2023-09-02 15:51:56 +0100

    test: added test cases

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    test_quote = quotes[0]
+    predicted_result = (test_quote['top_ask']['price']+test_quote['top_bid']['price'])/2
+    price_result = getDataPoint(test_quote)
+    self.assertEqual(price_result[3], predicted_result)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +19,32 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    test_quote = quotes[0]
+    stock, bid_price, ask_price, price = getDataPoint(test_quote)
+    self.assertEqual(bid_price > ask_price, True)
 
 
   """ ------------ Add more unit tests ------------ """
+
+  def test_getRatio_whenPriceBZero(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    price1 = getDataPoint(quotes[0])[3]
+    price2 = 0
+    predicted_ratio = None
+    self.assertEqual(getRatio(price1, price2), predicted_ratio)
+
+  def test_getRatio_whenPriceBNotZero(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    price1 = getDataPoint(quotes[0])[3]
+    price2 = getDataPoint(quotes[1])[3]
+    predicted_ratio = price1/price2
+    self.assertEqual(getRatio(price1, price2), predicted_ratio)
 
 
 
